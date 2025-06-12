3792ab13c0f2bc4c0ffa49efaa78eb890a4557ff
Felipe Riaño <143687093+FelipeR6@users.noreply.github.com>
2025-06-12 03:42:52 -0500

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,12 +2,15 @@
 const dotenv = require("dotenv")
 const path = require("path")
 
+// Cargar variables de entorno desde el archivo .env
+dotenv.config({ path: path.resolve(__dirname, "../.env") })
+
 module.exports = {
-  port: process.env.PORT,
+  port: process.env.PORT || 5000,
   dbConfig: {
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USER ,
-    password: process.env.DB_PASSWORD ,
-    database: process.env.DB_NAME 
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "sgit",
   },
 }
