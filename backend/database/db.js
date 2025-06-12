ffce35058f4b062139e0ca531476bf21dd2bd8fc
Felipe Riaño <143687093+FelipeR6@users.noreply.github.com>
2025-06-12 03:29:59 -0500

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'sgit'
+  host: "sgit.mysql.database.azure.com",
+  user: "useradmin",
+  password: "Sgit.2025",
+  database: "sgit"
 });
 
 // Prueba de conexión
