421bcfc4c0d9d601e72cdc87221cf3bda94801af
Felipe Riaño <143687093+FelipeR6@users.noreply.github.com>
2025-06-12 03:39:31 -0500

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,13 +1,19 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: "sgit.mysql.database.azure.com",
-  user: "useradmin",
-  password: "Sgit.2025",
-  database: "sgit"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: true, // o false si falla
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-// Prueba de conexión
 db.getConnection()
   .then(() => console.log('✅ Conectado a la base de datos SGIT'))
   .catch(err => console.error('❌ Error de conexión:', err.message));
