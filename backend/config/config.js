ed0f876ede8303ab41d61a1fa4662ca08070becb
Felipe Riaño <143687093+FelipeR6@users.noreply.github.com>
2025-06-12 03:25:55 -0500

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,11 +6,11 @@ const path = require("path")
 dotenv.config({ path: path.resolve(__dirname, "../.env") })
 
 module.exports = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT,
   dbConfig: {
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "sgit",
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME 
   },
 }
